19486252f3ae28dea0e966842008810c1cd0704b
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-02-13 23:52:49 +0200

    add statistics tracking

--- lab1/generators.py
+++ lab1/generators.py
@@ -8,7 +8,12 @@ def incremental_counter(start=1):
 
 def timeout_iterator(iterator, timeout):
     start_time = time.time()
+    total_sum = 0
+    count = 0
 
     while time.time() - start_time < timeout:
         value = next(iterator)
-        print(f"Value: {value}")
+        total_sum += value
+        count += 1
+        average = total_sum / count
+        print(f"Value: {value}, Sum: {total_sum}, Average: {average}")
