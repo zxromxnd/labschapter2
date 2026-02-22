8810c8fd4154eb50e83869578e4e3db180b2b13f
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-02-22 22:47:36 +0200

    add basic memoization decorator

--- lab3/memoization.py
+++ lab3/memoization.py
@@ -1,1 +1,12 @@
-# memoization decorator imp.
+def memoize(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        
+        result = func(*args)
+        cache[args] = result
+        return result
+    
+    return wrapper
