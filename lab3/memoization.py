65fe4ef992d6bfade8b75e939db2ea05ea9c8f74
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-02-26 14:20:00 +0200

    implement LFU strategy and strategy selection (LRU and LFU)

--- lab3/memoization.py
+++ lab3/memoization.py
@@ -1,24 +1,36 @@
 from collections import OrderedDict
 
-def memoize(func=None, max_size=None):
+def memoize(func=None, max_size=None, strategy='lru'):
     def decorator(f):
         cache = OrderedDict()
+        frequency = {}
 
         def wrapper(*args):
             if args in cache:
-                cache.move_to_end(args)
+                if strategy == 'lru':
+                    cache.move_to_end(args)
+                elif strategy == 'lfu':
+                    frequency[args] += 1
                 return cache[args]
             
             result = f(*args)
             cache[args] = result
 
+            if strategy == 'lfu':
+                frequency[args] = 1
+
             if max_size and len(cache) > max_size:
-                cache.popitem(last=False)
+                if strategy == 'lru':
+                    cache.popitem(last=False)
+                elif strategy == 'lfu':
+                    lfu_key = min(frequency, key=frequency.get)
+                    del cache[lfu_key]
+                    del frequency[lfu_key]
 
             return result
-        
         return wrapper
     if func is None:
         return decorator
     else:
         return decorator(func)
+                
