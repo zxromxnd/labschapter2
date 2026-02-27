183575dacaf1cb45d4ef2b7bacd32ea1b1964f9c
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-02-27 21:33:42 +0200

    add tests for LFU strategy and edit the 6th line on test_lru.py

--- lab3/test_lfu.py
+++ lab3/test_lfu.py
@@ -1,9 +1,9 @@
 from memoization import memoize
 
-def test_lru_basic():
+def test_lfu_basic():
     call_count = 0
 
-    @memoize(max_size=3, strategy='lru')
+    @memoize(max_size=3, strategy='lfu')
     def expensive_func(x):
         nonlocal call_count
         call_count += 1
@@ -12,15 +12,20 @@ def test_lru_basic():
     expensive_func(1)
     expensive_func(2)
     expensive_func(3)
+    assert call_count == 3
 
     expensive_func(1)
+    expensive_func(1)
+    expensive_func(3)
     assert call_count == 3
 
     expensive_func(4)
+    assert call_count == 4
+
     expensive_func(2)
     assert call_count == 5
 
-    print("LRU test passed.")
+    print("LFU test passed.")
 
 if __name__ == "__main__":
-    test_lru_basic()
+    test_lfu_basic()
