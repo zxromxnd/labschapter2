1131db0e06d5b756059d621a66ce779cd7477d45
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-01 19:50:14 +0200

    implement time-based expiry strategy

--- lab3/memoization.py
+++ lab3/memoization.py
@@ -1,23 +1,39 @@
 from collections import OrderedDict
+import time
 
-def memoize(func=None, max_size=None, strategy='lru'):
+def memoize(func=None, max_size=None, strategy='lru', ttl=None):
     def decorator(f):
         cache = OrderedDict()
         frequency = {}
+        timestamps = {}
 
         def wrapper(*args):
+            current_time = time.time()
+            
             if args in cache:
-                if strategy == 'lru':
+                if strategy == 'time' and ttl:
+                    if current_time - timestamps[args] > ttl:
+                        del cache[args]
+                        del timestamps[args]
+                    else:
+                        return cache[args]
+                elif strategy == 'lru':
                     cache.move_to_end(args)
+                    return cache[args]
                 elif strategy == 'lfu':
                     frequency[args] += 1
-                return cache[args]
+                    return cache[args]
+                else:
+                    return cache[args]
             
             result = f(*args)
             cache[args] = result
 
             if strategy == 'lfu':
                 frequency[args] = 1
+
+            if strategy == 'time':
+                timestamps[args] = current_time
 
             if max_size and len(cache) > max_size:
                 if strategy == 'lru':
@@ -28,9 +44,10 @@ def memoize(func=None, max_size=None, strategy='lru'):
                     del frequency[lfu_key]
 
             return result
+        
         return wrapper
+    
     if func is None:
         return decorator
     else:
         return decorator(func)
-                
