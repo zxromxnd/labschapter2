b7e5966d21ed0095fc0e83a1e2716c766d9716d0
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-02 00:51:52 +0200

    fix time-based expiry logic

--- lab3/memoization.py
+++ lab3/memoization.py
@@ -1,40 +1,37 @@
 from collections import OrderedDict
 import time
 
+
 def memoize(func=None, max_size=None, strategy='lru', ttl=None):
     def decorator(f):
         cache = OrderedDict()
         frequency = {}
         timestamps = {}
-
+        
         def wrapper(*args):
             current_time = time.time()
             
+            if args in cache and strategy == 'time' and ttl:
+                if current_time - timestamps[args] > ttl:
+                    del cache[args]
+                    del timestamps[args]
+            
             if args in cache:
-                if strategy == 'time' and ttl:
-                    if current_time - timestamps[args] > ttl:
-                        del cache[args]
-                        del timestamps[args]
-                    else:
-                        return cache[args]
-                elif strategy == 'lru':
+                if strategy == 'lru':
                     cache.move_to_end(args)
-                    return cache[args]
                 elif strategy == 'lfu':
                     frequency[args] += 1
-                    return cache[args]
-                else:
-                    return cache[args]
+                return cache[args]
             
             result = f(*args)
             cache[args] = result
-
+            
             if strategy == 'lfu':
                 frequency[args] = 1
-
+            
             if strategy == 'time':
                 timestamps[args] = current_time
-
+            
             if max_size and len(cache) > max_size:
                 if strategy == 'lru':
                     cache.popitem(last=False)
@@ -42,7 +39,7 @@ def memoize(func=None, max_size=None, strategy='lru', ttl=None):
                     lfu_key = min(frequency, key=frequency.get)
                     del cache[lfu_key]
                     del frequency[lfu_key]
-
+            
             return result
         
         return wrapper
