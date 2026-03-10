6e820506ca8de2bb5caf2f61685b1c9a358bce0e
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-10 11:47:17 +0200

    implement basic queue with enqueue

--- lab4/priority_queue.py
+++ lab4/priority_queue.py
@@ -1,1 +1,18 @@
-# Bi-Directional Priority Queue implementation
+class BiDirectionalPriorityQueue:
+    def __init__(self):
+        self.items = []
+        self.counter = 0
+    
+    def enqueue(self, item, priority):
+        self.items.append({
+            'item': item,
+            'priority': priority,
+            'order': self.counter
+        })
+        self.counter += 1
+
+    def size(self):
+        return len(self.items)
+    
+    def is_empty(self):
+        return len(self.items) == 0
