f9fde9efd483d1df7d8f60565d8f8f0677aaaa33
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-11 22:15:56 +0200

    add priority-based peek operations

--- lab4/priority_queue.py
+++ lab4/priority_queue.py
@@ -16,3 +16,25 @@ class BiDirectionalPriorityQueue:
     
     def is_empty(self):
         return len(self.items) == 0
+    
+    def peek(self, highest=False, lowest=False, oldest=False, newest=False):
+        if self.is_empty():
+            return None
+        
+        if highest:
+            max_item = max(self.items, key=lambda x: x['priority'])
+            return max_item['item']
+        
+        elif lowest:
+            min_item = min(self.items, key=lambda x: x['priority'])
+            return min_item['item']
+        
+        elif oldest:
+            min_order = min(self.items, key=lambda x: x['order'])
+            return min_order['item']
+        
+        elif newest:
+            max_order = max(self.items, key=lambda x: x['order'])
+            return max_order['item']
+        
+        return None
