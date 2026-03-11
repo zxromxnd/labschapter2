abc6fb3884ef5e3e62cc0caf0c19630c26cc0b0b
zxromxnd <zhenya.bryukhovetsky@gmail.com>
2026-03-12 00:25:38 +0200

    add priority-based dequeue operations

--- lab4/priority_queue.py
+++ lab4/priority_queue.py
@@ -38,3 +38,29 @@ class BiDirectionalPriorityQueue:
             return max_order['item']
         
         return None
+    
+    def dequeue(self, highest=False, lowest=False, oldest=False, newest=False):
+        if self.is_empty():
+            return None
+        
+        if highest:
+            max_item = max(self.items, key=lambda x: x['priority'])
+            self.items.remove(max_item)
+            return max_item['item']
+        
+        elif lowest:
+            min_item = min(self.items, key=lambda x: x['priority'])
+            self.items.remove(min_item)
+            return min_item['item']
+        
+        elif oldest:
+            min_order = min(self.items, key=lambda x: x['order'])
+            self.items.remove(min_order)
+            return min_order['item']
+        
+        elif newest:
+            max_order = max(self.items, key=lambda x: x['order'])
+            self.items.remove(max_order)
+            return max_order['item']
+        
+        return None
